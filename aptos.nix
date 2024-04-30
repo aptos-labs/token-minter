290df7e20abd345ba4af1ddbb64245e5c5f02e3a
John C <johnchang9094@gmail.com>
2024-04-29 17:49:00 -0700

    Update Aptos CLI to 3.1.0 in build (#59)

--- aptos.nix
+++ aptos.nix
@@ -7,11 +7,11 @@ let
     else throw "Unsupported platform ${stdenv.system}";
 
   sha256 = if os == "MacOSX" then "sha256-6uVxfgcFgxgwF9vwQwaTt23IbV5vNACmjN9BO6TSQ20="
-            else "sha256-m5wcgXLqoONKiI+7pbwCVhnsKmvjZVS/f1S9sOascF8=";
+            else "sha256-L/rpf/94WcK4Im5mgfFXypD16U+iVqkGmeOShx7MrH0=";
 
 in stdenv.mkDerivation rec {
   pname = "aptos-cli";
-  version = "2.1.1";
+  version = "3.1.0";
 
   src = fetchurl {
     url = "https://github.com/aptos-labs/aptos-core/releases/download/${pname}-v${version}/${pname}-${version}-${os}-x86_64.zip";
